7bd2b9e066d053d71d34a187106b6806fea8dc05
Jim Kalafut <jim@kalafut.net>
2014-10-25 14:27:09 -0700

    Before declassification

--- taskbug.py
+++ taskbug.py
@@ -1,30 +1,41 @@
+#!/usr/bin/env python
+
 import os
 import readline
 history = []
 
 commands = {}
+tracks=[ [] ]
+track = tracks[0]
 
 class Track:
     def __init__(self, task=None):
         self.tasks = []
         self.push(task)
 
-
     def push(self, task):
         if task:
             self.tasks.append(task)
 
     def pop(self):
-        return self.tasks.pop()
+        if not self.is_empty():
+            return self.tasks.pop()
 
     def top(self):
-        if self.count() > 0:
+        if not self.is_empty():
             return self.tasks[-1]
         else:
             return None
 
     def count(self):
         return len(self.tasks)
+
+    def list(self):
+        for t in self.tasks:
+            print t
+
+    def is_empty(self):
+        return self.count() == 0
 
 class TrackCollection:
     def __init__(self):
@@ -67,36 +78,31 @@ def help(line):
     for k,f in commands.iteritems():
         print "{} - {}".format(k, f[1])
 
+@command('__DEFAULT__')
+def add(line):
+    t = tc.current()
+    t.push(line)
+
+
+@command('d')
+def drop(line):
+    t = tc.current()
+    t.pop()
+
+@command('l')
+def list(line):
+    tc.current().list()
 
 def parse(line):
     for k,f in commands.iteritems():
         if line.startswith(k):
             f[0](line)
-
-routes = {
-    'q': quit,
-    'c': clear
-    }
-
+            return
+    commands['__DEFAULT__'][0](line)
 
 while True:
     t = tc.current()
     print t.top()
-    cmd = raw_input('{} >'.format(t.count()))
+    cmd = raw_input('{} > '.format(t.count()))
     parse(cmd)
-    # if cmd == "done":
-    #     history.append(t.pop())
-    # elif cmd == "hist":
-    #     print history
-    # elif cmd.startswith("n "):
-    #     tc.add(cmd)
-    # elif cmd == "clear":
-    #     os.system("clear")
-    # elif cmd == "q":
-    #     pass
-
-
-    # else:
-    #     t.push(cmd)
-
     print
